e9cf3f60d73b1eb26e8d61e4958ddb3dd095c8b0
Eric Cestari <ecestari@mac.com>
2017-01-06 19:06:45 +0100

    Send correct code

--- lib/apns_handler.ex
+++ lib/apns_handler.ex
@@ -13,8 +13,17 @@ defmodule ApnsHandler do
         reply_error(405, "MethodNotAllowed", req, state)
     end
 
+    def prepare_reply(_body, _headers, token, req, state) when token == :undefined do
+        reply_error(400, "MissingDeviceToken", req, state) 
+    end
+
+    def prepare_reply(_body, _headers, "CustomError=" <> error, req, state) do
+        {code, error} = get_code(error)
+        reply_error(code, error, req, state)
+    end
+
     def prepare_reply(body, %{"apns-id" => apns_id} = headers, token, req, state) do
-        req = :cowboy_req.reply 200, %{"content-type" => "application/json", "apns-id" => apns_id}, "Hello XWorld", req
+        req = :cowboy_req.reply 200, %{"content-type" => "application/json", "apns-id" => apns_id}, "", req
         {:ok, req, state}
     end
 
@@ -27,6 +36,40 @@ defmodule ApnsHandler do
         {:ok, body} = Poison.encode %{reason: reason}
         req = :cowboy_req.reply code, %{"content-type" => "application/json"}, body, req
         {:ok, req, state}
+    end
+
+    def get_code(error) do
+        case error do
+            "BadCollapseId" = error -> {400, error}
+            "BadDeviceToken" = error -> {400, error}
+            "BadExpirationDate" = error -> {400, error}
+            "BadMessageId" = error -> {400, error}
+            "BadPriority" = error -> {400, error}
+            "BadTopic" = error -> {400, error}
+            "DeviceTokenNotForTopic" = error -> {400, error}
+            "DuplicateHeaders" = error -> {400, error}
+            "IdleTimeout" = error -> {400, error}
+            "MissingDeviceToken" = error -> {400, error}
+            "MissingTopic" = error -> {400, error}
+            "PayloadEmpty" = error -> {400, error}
+            "TopicDisallowed" = error -> {400, error}
+            "BadCertificate" = error -> {403, error}
+            "BadCertificateEnvironment" = error -> {403, error}
+            "ExpiredProviderToken" = error -> {403, error}
+            "Forbidden" = error -> {403, error}
+            "InvalidProviderToken" = error -> {403, error}
+            "MissingProviderToken" = error -> {403, error}
+            "BadPath" = error -> {404, error}
+            "MethodNotAllowed" = error -> {405, error}
+            "Unregistered" = error -> {410, error}
+            "PayloadTooLarge" = error -> {413, error}
+            "TooManyProviderTokenUpdates" = error -> {429, error}
+            "TooManyRequests" = error -> {429, error}
+            "InternalServerError" = error -> {500, error}
+            "ServiceUnavailable" = error -> {503, error}
+            "Shutdown" = error -> {503, error}
+            _ = error -> {400, "PayloadEmpty"}
+        end
     end
 end
 
