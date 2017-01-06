0c77b31bdb5de39c2a410c3057dbb2c651dd9f2a
Eric Cestari <ecestari@mac.com>
2017-01-06 19:06:53 +0100

    Refactor client

--- lib/h2_client.ex
+++ lib/h2_client.ex
@@ -1,31 +1,11 @@
-#defmodule H2Client do
-#
-#    def request() do
-#        {:ok, pid} = Kadabra.open('localhost', :https, port: 8443)
-#    
-#        path = "/3/device/toto" # Route echoes PUT body in uppercase
-#        body = "sample echo request"
-#        headers = [
-#          {":method", "POST"},
-#          {"aps-id", "toto"},
-#          {":path", path},
-#        ]
-#    
-#        Kadabra.request(pid, headers, body)
-#    
-#        receive do
-#          {:end_stream, %Kadabra.Stream{} = stream} ->
-#          IO.inspect stream
-#        after 5_000 ->
-#          IO.puts "Connection timed out."
-#        end
-#    end
-#end
-
 defmodule H2Client do
 
-    def request(host, port, headers, body, opts \\ []) do
+    def init(host, port, opts \\ []) do
         {:ok, pid} = Kadabra.open(host,  port, opts)
+        pid
+    end
+
+    def request(pid, headers, body) do
         IO.puts "pid = #{inspect pid}"
         res =  Kadabra.request(pid, headers, body)
         IO.puts "res = #{inspect res}"
@@ -37,13 +17,31 @@ defmodule H2Client do
         end
     end
 
-    def local do
+    def request(host, port, headers, body, opts \\ []) do
+       pid = init(host,  port, opts)
+       request(pid, headers, body)
+    end
+
+    def init_local() do
+        init('localhost', :https, verify: :verify_none, port: 8443)
+    end
+
+    def local(pid) do
         headers =  [
           {":method", "POST"},
           {"apns-id", "toto"},
           {":path", "/3/device/toto"},
         ]
-        request('localhost', :https, headers, "toto", verify: :verify_none, port: 8443)
+        request(pid, headers, "toto")
+    end
+
+    def local_error(pid, message) do
+        headers =  [
+          {":method", "POST"},
+          {"apns-id", "toto"},
+          {":path", "/3/device/CustomError=#{message}"},
+        ]
+        request(pid, headers, "toto")
     end
 
     def golang do
